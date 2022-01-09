4f2fae1ee656964df562bfe8a2da2f143e5064cc
Jonas324 <jonaswadin24@gmail.com>
2022-01-09 18:46:22 +0100

    hejjj

--- Inlämning1/src/main/java/Squirrel.java
+++ Inlämning1/src/main/java/Squirrel.java
@@ -1,25 +1,2 @@
 public class Squirrel {
-
-    private int weight;
-    private int numOfConesInNest;
-    private int age;
-
-    public Squirrel(int weight, int numOfConesInNest, int  age){
-
-        this.weight = weight;
-        this.numOfConesInNest = numOfConesInNest;
-        this.age = age;
-
-
-    }
-
-    public int getWeight(){ return weight;}
-    public void setWeight(int i){weight = i; }
-
-    public int getNumOfConesInNest(){ return numOfConesInNest;}
-    public void setNumOfConesInNest(int i){numOfConesInNest = i; }
-
-    public int getAge(){ return age;}
-    public void setAge(int i){age = i; }
-
 }
