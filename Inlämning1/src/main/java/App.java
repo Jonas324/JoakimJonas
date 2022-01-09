233c1be072a8a5d79513879b25bbb0d774188bac
Jonas324 <jonaswadin24@gmail.com>
2022-01-09 19:36:05 +0100

    hejjj

--- Inlämning1/src/main/java/App.java
+++ Inlämning1/src/main/java/App.java
@@ -1,7 +1,15 @@
+import java.net.StandardSocketOptions;
+
 public class App {
 
     public static void main(String[] args) {
 
-        
+        int age = 10;
+        int numOfConesInNest = 20;
+        int weight = 30;
+
+        Squirrel sqr = new Squirrel(age, numOfConesInNest, weight);
+
+        System.out.println(sqr.getWeightAge());
     }
 }
