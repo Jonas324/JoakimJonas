0cc740c7b71631bc473de84fdde3cc5b8b6d7055
Jonas324 <jonaswadin24@gmail.com>
2022-01-10 20:58:26 +0100

    hejjj

--- Inlämning1/src/main/java/App.java
+++ Inlämning1/src/main/java/App.java
@@ -8,9 +8,17 @@ public class App {
         int numOfConesInNest = 20;
         int weight = 30;
 
+        boolean hungry = true;
+        int wingSpan = 20;
+        int owlAge = 15;
+
         Squirrel sqr = new Squirrel(age, numOfConesInNest, weight);
 
         System.out.println(sqr.getWeightAge());
+
+        Owl owl = new Owl(wingSpan, hungry, owlAge);
+
+        System.out.println(owl.getFed());
     }
 }
 
--- Inlämning1/src/main/java/Owl.java
+++ Inlämning1/src/main/java/Owl.java
@@ -1,2 +1,10 @@
 public class Owl {
+    public boolean fed;
+
+    public Owl(int wingSpan, boolean hungry, int owlAge) {
+    }
+
+    public boolean getFed() {
+        return fed;
+    }
 }
